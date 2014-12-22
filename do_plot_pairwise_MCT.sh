234b460edf66a5589631ec9141d9f983c783b910
Christopher Whidden <cwhidden@fredhutch.org>
2014-12-22 10:38:30 -0800

    pairwise commute times with 4 and 5-taxon 1v5c and equal

--- do_plot_pairwise_MCT.sh
+++ do_plot_pairwise_MCT.sh
@@ -1,11 +1,15 @@
 in_file=`mktemp`;
 for file in `find walks/random_walks/ -name 'commute_time_all*'`; do
-	name=`echo $file | sed 's/\/commute_time_all//; s/^.*\///'`;
-	out_file=figs/$name-commute-all
 	echo $file;
+	name=`echo $file | sed 's/\/commute_time_all_[0-9]*//; s/^.*\///'`;
+	out_file=figs/$name-commute-all
+	interesting_file=interesting_trees/${name}.rooted.tre
+	if [ ! -f $interesting_file ]; then
+		interesting_file=""
+	fi
 	echo "num1,num2,MAT,MATERR,MCT,MCTERR" > $in_file;
 	cat $file >> $in_file;
-	R --vanilla < plot_MCT.r --args $in_file $out_file
+	R --vanilla < plot_MCT.r --args $in_file $out_file $interesting_file
 	echo ${out_file}.pdf
 	echo;
 done
--- plot_MCT.r
+++ plot_MCT.r
@@ -1,14 +1,24 @@
 args <- commandArgs(trailingOnly = TRUE)
 in_file <- args[1];
 out_file <- args[2];
+tree_num_file <- args[3];
+
 
 cls <- c(distance="numeric", curvature="numeric")
 mydata <- read.csv(in_file, sep=",", colClasses=cls);
+
+do_interesting_trees = FALSE;
+if (file.exists(tree_num_file)) {
+	interesting_trees <- read.csv(tree_num_file, sep=",", header = FALSE)
+	do_interesting_trees = TRUE;
+}
 
 pdf(paste(out_file, ".pdf", sep=""));
 
 require(ggplot2)
 require(scales)
+require(reshape)
+require(plyr)
 
 theme_set(theme_bw(16) +
 	theme(strip.background=element_blank(),
@@ -30,8 +40,50 @@ limits <- aes(ymax = MCT + MCTERR, ymin = MCT - MCTERR)
 #p + geom_point(size=2) +
 #		geom_errorbar(limits)
 
-p <- ggplot(mydata, aes(x = MCT))
-p + geom_density()
+p <- ggplot(mydata, aes(x = MCT)) +
+	geom_density()
+print(p);
 
+p <- ggplot(mydata, aes(x = num1, y = num2)) +
+	geom_tile(aes(fill = MCT), colour = "white") +
+	scale_fill_gradient(low = "white", high="steelblue") +
+	xlab("tree 1") +
+	ylab("tree 2")
+print(p);
+
+p <- ggplot(mydata, aes(x = num1, y = num2)) +
+	geom_tile(aes(fill = MAT), colour = "white") +
+	scale_fill_gradient(low = "white", high="steelblue") +
+	xlab("tree 1") +
+	ylab("tree 2")
+print(p);
+
+if (do_interesting_trees) {
+
+mydata_subset = subset(mydata,
+		mydata$num1 %in% interesting_trees$V1 &
+		mydata$num2 %in% interesting_trees$V1)
+
+mydata_subset$num1=as.factor(mydata_subset$num1)
+mydata_subset$num2=as.factor(mydata_subset$num2)
+
+p <- ggplot(mydata_subset, aes(x = num1, y = num2)) +
+	geom_tile(aes(fill = MCT), colour = "white") +
+	scale_fill_gradient(low = "white", high="steelblue") +
+	xlab("tree 1") +
+	ylab("tree 2") +
+	ggtitle("MCT (most likely trees)");
+print(p)
+
+p <- ggplot(mydata_subset, aes(x = num1, y = num2)) +
+	geom_tile(aes(fill = MAT), colour = "white") +
+	scale_fill_gradient(low = "white", high="steelblue") +
+	xlab("tree 1") +
+	ylab("tree 2") +
+	ggtitle("MAT (most likely trees)");
+print(p);
+
+}
+	
 dev.off()
 quit()
